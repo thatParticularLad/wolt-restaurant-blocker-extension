031a05e6df5f77ac09218e8282ecf74caef8c895
Adomas <sadasdasdas2312@gmail.com>
2021-03-07 19:59:19 +0200

    removed unused code

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 //placeholder blocked restaurants
 const blockedNames = ['grill-food', 'doublepizza']
 
-// // Select the node that will be observed for mutations
-// const mainContent = document.getElementById('mainContent');
-
-// // Options for the observer (which mutations to observe)
-// const config = { attributes: false, childList: true, subtree: true };
-
-// // Callback function to execute when mutations are observed
-// const callback = function(mutationsList, observer) {
-//     for(const mutation of mutationsList) {
-//         if (mutation.type === 'childList') {
-//             filterRestaurants()
-//         }
-//     }
-// };
-
 function getRestaurants(){
         const restaurants = document.querySelectorAll(`[href*='ltu/vilnius/restaurant/']`);
         return restaurants 
@@ -51,12 +36,6 @@ window.onload = function(){
     
 }
 window.addEventListener("click", filterRestaurants());
-// Create an observer instance linked to the callback function
-//  which filters the restaurants
-// const observer = new MutationObserver(callback);
-
-// // Start observing the target node for configured mutations
-// observer.observe(mainContent, config);
 
 setInterval(filterRestaurants, 500);
 
